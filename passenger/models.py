9f5959fd3cec65dbe54769cd3f0332a579549e0a
artur-mrozek <artur.mrozek1@gmail.com>
2023-03-29 13:21:25 +0200

    foreign key to passenger

--- passenger/models.py
+++ passenger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,3 +7,4 @@ class Passenger(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     passport_number = models.CharField(max_length=8)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
