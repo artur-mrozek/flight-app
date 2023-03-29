a8d5254cec0bbacbd8df5dfbd9e7661a4e53d227
artur-mrozek <artur.mrozek1@gmail.com>
2023-03-29 13:32:54 +0200

    login url

--- passenger/views.py
+++ passenger/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request, *args, **kwargs):
+    context = {}
+    return render(request, "passenger/login.html", context)
