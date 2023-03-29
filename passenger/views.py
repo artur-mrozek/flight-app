55108bd29f3c491fb4ef59c99ea1aec48e39f1be
artur-mrozek <artur.mrozek1@gmail.com>
2023-03-29 13:33:53 +0200

    register url

--- passenger/views.py
+++ passenger/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def login_view(request, *args, **kwargs):
     context = {}
     return render(request, "passenger/login.html", context)
+
+def register_view(request, *args, **kwargs):
+    context = {}
+    return render(request, "passenger/register.html", context)
