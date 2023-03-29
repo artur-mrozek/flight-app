f2c8b987469edf3193c180dc80d6c3a19a69e97b
artur-mrozek <artur.mrozek1@gmail.com>
2023-03-29 13:55:09 +0200

    redirect not logged in users to login page

--- flight/views.py
+++ flight/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 def flights_list_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("/passenger/login")
     context = {}
     return render(request, "flight/flights_list.html")
