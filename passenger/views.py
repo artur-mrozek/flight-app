41ade921cf4b254b74ba9f1229b6557b1f4505f8
artur-mrozek <artur.mrozek1@gmail.com>
2023-03-29 13:48:49 +0200

    login page

--- passenger/views.py
+++ passenger/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 def login_view(request, *args, **kwargs):
-    context = {}
+    if request.user.is_authenticated:
+        return redirect("/flights/")
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        user = authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+            return redirect('/flights/')
+        else:
+            return redirect('/passenger/login/')
+    context = {"form": form}
     return render(request, "passenger/login.html", context)
 
 def register_view(request, *args, **kwargs):
