356906f9745274cb9245631f82c06a80d1ee77cc
artur-mrozek <artur.mrozek1@gmail.com>
2023-03-29 13:38:22 +0200

    buy ticket url

--- flight_app/urls.py
+++ flight_app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('passenger/', include('passenger.urls')),
+    path('ticket/', include('ticket.urls')),
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def buy_ticket_view(request, *args, **kwargs):
+    context = {}
+    return render(request, "ticket/buy_ticket.html", context)
